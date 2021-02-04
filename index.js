500a52d0b689f9dda904b776516328ddf86029b8
Steven Israel <malenchite@gmail.com>
2021-02-04 09:24:41 -0500

    Implement inquirer questions and prompts

--- index.js
+++ index.js
@@ -2,33 +2,138 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const render = require("./src/renderTeamPage");
 
-/* TODO: create Inquirer questions list */
-const questions = [];
+/* Questions to ask to construct manager data */
+const managerQuestions = [
+  {
+    type: "input",
+    message: "What is the team manager's name?",
+    name: "name"
+  },
+  {
+    type: "input",
+    message: "What is the team manager's employee ID?",
+    name: "id"
+  },
+  {
+    type: "input",
+    message: "What is the team manager's email address?",
+    name: "email"
+  },
+  {
+    type: "input",
+    message: "What is the team manager's office number?",
+    name: "office"
+  },
+];
+
+/* Questions to ask for additional employees */
+const employeeQuestions = [
+  {
+    type: "list",
+    message: "Add an additional employee?",
+    choices: ["Engineer", "Intern", "Finish"],
+    name: "choice"
+  },
+  {
+    type: "input",
+    message: "What is the employee's name?",
+    name: "name",
+    when: answers => answers.choice !== "Finish"
+  },
+  {
+    type: "input",
+    message: "What is the employee's ID?",
+    name: "id",
+    when: answers => answers.choice !== "Finish"
+  },
+  {
+    type: "input",
+    message: "What is the employee's email address?",
+    name: "email",
+    when: answers => answers.choice !== "Finish"
+  },
+  {
+    type: "input",
+    message: "What is the engineer's GitHub user name?",
+    name: "github",
+    when: answers => answers.choice === "Engineer"
+  },
+  {
+    type: "input",
+    message: "Where does the intern go to school?",
+    name: "github",
+    when: answers => answers.choice === "Intern"
+  },
+];
+
+/* Container for Employee objects */
+var employeeData = {
+  manager: null,
+  engineers: [],
+  interns: []
+};
+
+/* Saves HTML and CSS files based on employee data */
+function saveFiles(dir) {
+  /* Write HTML to file */
+  fs.writeFile(dir + "/index.html", render.teamPage(employeeData), err => {
+    err ? console.log(err) : console.log("HTML file written successfully.")
+  });
+
+  /* Write CSS to file */
+  fs.writeFile(dir + "/style.css", render.CSS(), err => {
+    err ? console.log(err) : console.log("CSS file written successfully.");
+  });
+}
+
+/* Function to prompt for employee data, recursively called for additional employees */
+function employeePrompt(dir) {
+  inquirer.prompt(employeeQuestions)
+    .then(answers => {
+      /* Process new employee data */
+      if (answers.choice !== "Finish") {
+        let newEmployee;
+        switch (answers.choice) {
+          case "Engineer":
+            /* TODO: Construct Engineer object */
+            employeeData.engineers.push(newEmployee);
+            break;
+          case "Intern":
+            /* TODO: Construct Intern object */
+            employeeData.interns.push(newEmployee);
+            break;
+        }
+        /* Ask if user wants to add another employee */
+        employeePrompt(dir);
+      } else {
+        /* Save files and complete */
+        saveFiles(dir);
+        console.log(employeeData);
+      }
+    });
+}
 
 /* Initializes app and starts prompt */
 function init() {
-  var dir = './dist';
+  /* Directory to save data to */
+  var dir = "./dist";
 
   // Create output directory if it doesn't already exist
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
 
-  inquirer.prompt(questions)
+  /* Prompt for manager data */
+  inquirer.prompt(managerQuestions)
     .then(answers => {
-      /* TODO: Process answers into data for team page render */
-      let data = "";
+      let manager;
+      /* TODO: Construct Manager object */
+      employeeData.manager = manager;
 
-      /* Write HTML to file */
-      fs.writeFile(dir + "/index.html", render.teamPage(data), err => {
-        err ? console.log(err) : console.log("HTML file written successfully.")
-      });
-
-      /* Write CSS to file */
-      fs.writeFile(dir + "/style.css", render.CSS(), err => {
-        err ? console.log(err) : console.log("CSS file written successfully.");
-      });
+      /* Prompt for additional employee data, saving data when complete*/
+      employeePrompt(dir);
     });
+
 }
 
 init();
